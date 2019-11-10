5b57bbac7938fac1fac44c6ba4df0e82c709edd9
Ulan Rakymzhanov <ulan@carta.healthcare>
2019-11-09 17:24:00 -0800

    added responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import '../App.css'
 
-const image = {
-      width: 30,
-      height: 30,
-      paddingLeft: 5
-};
 
 const Footer = () => {
     return (
     <footer>
-      <a href="https://www.linkedin.com/in/ulan-rakymzhanov/" target="_blank" rel="noopener noreferrer"><img src={require('../assets/linkedIn.svg')} alt="linkedin" style={image}/></a>
-      <a href="https://twitter.com/home" target="_blank" rel="noopener noreferrer"><img src={require('../assets/twitter.svg')} style={image} alt="twitter"/></a>
-      <a href="https://www.facebook.com/ulan13" target="_blank" rel="noopener noreferrer"><img src={require('../assets/facebook.svg')} style={image}alt="facebook" /></a>
-      <a href="https://www.instagram.com/u_rakymzhan/" target="_blank" rel="noopener noreferrer"><img src={require('../assets/instagram.svg')} alt="instagram" style={image}/></a>
+      <a href="https://www.linkedin.com/in/ulan-rakymzhanov/" target="_blank" rel="noopener noreferrer"><img src={require('../assets/linkedIn.svg')} className="footer-icon" alt="linkedin" /></a>
+      <a href="https://twitter.com/home" target="_blank" rel="noopener noreferrer"><img src={require('../assets/twitter.svg')} className="footer-icon" alt="twitter"/></a>
+      <a href="https://www.facebook.com/ulan13" target="_blank" rel="noopener noreferrer"><img src={require('../assets/facebook.svg')} className="footer-icon" alt="facebook" /></a>
+      <a href="https://www.instagram.com/u_rakymzhan/" target="_blank" rel="noopener noreferrer"><img src={require('../assets/instagram.svg')} className="footer-icon"  alt="instagram"/></a>
     </footer>
     );
   }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,24 +9,18 @@ import {
 import GettingStarted from './GettingStarted.jsx'
 import gif from '../assets/loading.gif'
 
-const image = {
-    width: 100,
-    height: 100,
-    borderRadius: 400/2
-};
-
 const Sidebar = () => {
     return (
     <Router>
         <div className="bodyContent">
             <div className="sidenav">
                 <div className="profile">
-                    <Link to="/"><img src={require('../assets/profile.jpg')} style={image} alt="profile"/></Link>
+                    <Link to="/"><img src={require('../assets/profile.jpg')} alt="profile" className="profile-img"/></Link>
                     <p>Ulan Rakymzhanov <br/> Personal Blog</p>
                 </div>
                 <ul>
                     <Link to="/about"><li>About</li></Link>
-                    <Link to="/family"><li>Family</li></Link>
+                    <Link to="/family"><li>Family ASDSAD AS cdcdcd  edaadaedd  dadad</li></Link>
                 </ul>
             </div>
             <div className="contents"> 
@@ -35,15 +29,15 @@ const Sidebar = () => {
                         <GettingStarted/>
                     </Route>
                     <Route path="/family">
-                        <div className="family" style={{fontSize: '25px'}}>
+                        <div className="family">
                             <p>Januzak, Bebe, Enes</p>
                             <p style={{color: '#FF0000'}}>Kana, Guli, Ayima</p>
                             <p>Kairat, Nuria, Azalia, Milana</p> 
                             <p style={{color: '#4169E1'}}>Topa, Ayok, Amalia</p>
                             <p>Kairat, Zhibek, future...</p>
                             <p style={{color: '#808000'}}>Hi from Ulan, Jazzy</p>
-                            </div>
-                            <img src='https://media.giphy.com/media/4adI6CB0g4KTomIbKx/giphy.gif' width='200px' height='200px' alt='waive'/>
+                            <img src='https://media.giphy.com/media/4adI6CB0g4KTomIbKx/giphy.gif' alt='waive'/>
+                        </div>
                     </Route>
                     <Route path="/">
                         <div>
