5feca65f7a627932c72ec70f718ceb467c0d0035
Ulan <ulanr92@gmail.com>
2025-01-12 17:33:16 -0600

    Updates

--- static/js/8.923bfdf6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppersonal-website"]=this["webpackJsonppersonal-website"]||[]).push([[8],{505:function(t,a,e){"use strict";e.d(a,"a",(function(){return s}));var n=e(0);function s(t){return Object(n.a)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M272 464a16 16 0 01-16-16.42V264.13a8 8 0 00-8-8H64.41a16.31 16.31 0 01-15.49-10.65 16 16 0 018.41-19.87l384-176.15a16 16 0 0121.22 21.19l-176 384A16 16 0 01272 464z"}}]})(t)}}}]);
-//# sourceMappingURL=8.923bfdf6.chunk.js.map
